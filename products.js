c12cb6a61e0c20e2626744d8c7b19dcab0f8ddb9
brunorodriguezviera <139410381+brunorodriguezv@users.noreply.github.com>
2023-08-16 09:18:43 -0300

    Nuevos cambios con funcion async y await

--- products.js
+++ products.js
@@ -1,19 +1,14 @@
 const productListElement = document.getElementById("product-list");
-
-
 const apiUrl = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 
+async function fetchProducts() {
+  try {
+    const response = await fetch(apiUrl);
+    const products = await response.json();
 
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    const products = data.products;
-
-
-    products.forEach(product => {
+    products.products.forEach(product => {
       const productElement = document.createElement("div");
       productElement.className = "product";
-
       productElement.innerHTML = `
         <img src="${product.image}" alt="${product.name}">
         <h2>${product.name}</h2>
@@ -21,10 +16,11 @@ fetch(apiUrl)
         <p>Descripción: ${product.description}</p>
         <p>Cantidad Vendidos: ${product.soldCount}</p>
       `;
-
       productListElement.appendChild(productElement);
     });
-  })
-  .catch(error => {
+  } catch (error) {
     console.error("Error al cargar los productos:", error);
-  });
+  }
+}
+
+fetchProducts();
