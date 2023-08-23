87232052932aad196fa98aa714e9f0cef6ebbd09
romero-guillermo <guilleliketio@gmail.com>
2023-08-23 11:24:13 -0300

    Cambios 23/8 (Entrega 2)
    Se agrega un archivo .js para linkear la función de cierre de sesión a todos los html.
    se linkean todos los .html con el .js logout.
    Se agrega el nombre de usuario a la Navbar al lado del logout.

--- js/index.js
+++ js/index.js
@@ -23,18 +23,3 @@ if (!isLoggedIn) {
       
 
 
-const logout = document.getElementById("logout")
-
-logout.addEventListener("click", ()=> {
-
-var confirmacion = confirm("¿Estás seguro de que deseas cerrar sesión?");
-if (confirmacion) {
-    localStorage.removeItem("isLoggedIn")
-    window.location.href = 'login.html'
-}
-
-})
-
-const nombreNav = document.getElementById("nombreNav");
-const usernameLocal = localStorage.getItem("username");
-nombreNav.textContent = usernameLocal;
