b17468b47d694c5082ddfef96553a79de69eb919
mmantaras <micaela.mantaras@gmail.com>
2023-08-30 10:05:25 -0300

    Se modifica el index.js para que se obtenga el nombre de la cat. del localStorage y pueda aparecer en el subtítulo desde el productos.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
+        localStorage.setItem("catName", "Autos");
         window.location = "products.html"
     });
     document.getElementById("juguetes").addEventListener("click", function() {
         localStorage.setItem("catID", 102);
+        localStorage.setItem("catName", "Juguetes");
         window.location = "products.html"
     });
     document.getElementById("muebles").addEventListener("click", function() {
         localStorage.setItem("catID", 103);
+        localStorage.setItem("catName", "Muebles");
         window.location = "products.html"
     });
 });
@@ -21,5 +24,4 @@ if (!isLoggedIn) {
     window.location.href = 'login.html';
 }
       
-
 
