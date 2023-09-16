f4bd1b3029923bb9c29f22185175989b33745248
romero-guillermo <guilleliketio@gmail.com>
2023-09-16 10:53:34 -0300

    resolviendo conflictos

--- js/product-info.js
+++ js/product-info.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const cont = document.getElementById("contenedor");
+
   const imageThumbnailsContainer = document.getElementById("image-thumbnails");
   const imagenAmpliada = document.getElementById("imagen-ampliada");
   const id = localStorage.getItem("id");
@@ -50,8 +51,9 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+// Seccion para los comentarios
+  let commentsArray = [];
 
-document.addEventListener("DOMContentLoaded", () => {
   const commentsContainer = document.getElementById("comments-container");
   const id = localStorage.getItem("id");
   const apiUrl = `https://japceibal.github.io/emercado-api/products_comments/${id}.json`;
@@ -69,36 +71,51 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   async function displayComments() {
-      const commentsData = await fetchComments(apiUrl);
 
-      if (commentsData.length === 0) {
-        commentsContainer.innerHTML = "Todavía no hay comentarios.";
-        return;
-      }
+    // Mostrar comentarios almacenados localmente
+    commentsArray.forEach((comment) => {
+      displayComment(comment);
+    });
 
-      commentsData.forEach(comment => {
-        const commentDiv = document.createElement("div");
-        commentDiv.classList.add("comment");
+    // Obtener comentarios de la API
+    const apiComments = await fetchComments(apiUrl);
 
-        const starRating = document.createElement("span");
-        starRating.classList.add("star-rating");
-        starRating.innerHTML = generateStarRating(comment.score);
-        commentDiv.appendChild(starRating);
+    if (apiComments.length === 0 && commentsArray.length === 0) {
+      // Manejar el caso en el que no hay comentarios.
+      commentsContainer.innerHTML = "Todavía no hay comentarios.";
+    } else {
+      // Mostrar comentarios de la API
+      apiComments.forEach((comment) => {
+        displayComment(comment);
 
-        commentDiv.innerHTML += `
-            <p>Producto: ${comment.product}</p>
-            <p>${comment.description}</p>
-            <p>-${comment.user}</p>
-            <p>${comment.dateTime}</p>
-        `;
-
-        commentsContainer.appendChild(commentDiv);
       });
     }
+  }
+
+  function displayComment(comment) {
+    const commentDiv = document.createElement("div");
+    commentDiv.classList.add("comment");
+
+    // Crea un elemento <span> para las estrellas
+    const starRating = document.createElement("span");
+    starRating.classList.add("star-rating");
+    starRating.innerHTML = generateStarRating(comment.score);
+    commentDiv.appendChild(starRating);
+
+    commentDiv.innerHTML += `
+        <p>Producto: ${comment.product}</p>
+        <p>${comment.description}</p>
+        <p>-${comment.user}</p>
+        <p>${comment.dateTime}</p>
+    `;
+
+    commentsContainer.appendChild(commentDiv);
+  }
 
 
-    function generateStarRating(score) {
-    const maxScore = 5; 
+  function generateStarRating(score) {
+    const maxScore = 5;
+
     const filledStars = '<i class="fa fa-star"></i>'.repeat(score);
     const emptyStars = '<i class="fa fa-star-o"></i>'.repeat(maxScore - score);
     return filledStars + emptyStars;
@@ -106,49 +123,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
   displayComments();
 
-  document.addEventListener("DOMContentLoaded", () => {
+  // Agrega un evento de escucha para el formulario de comentarios
+  const commentForm = document.getElementById("comment-form");
+  commentForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-    // Agrega un evento de escucha para el formulario de comentarios
-    const commentForm = document.getElementById("comment-form");
-    commentForm.addEventListener("submit", (e) => {
-        e.preventDefault(); // Evita la recarga de la página por defecto
+    const score = document.getElementById("score").value;
+    const commentText = document.getElementById("comment").value;
+  
+    const usernameLocal = localStorage.getItem("username");
+    
+    // Crea un nuevo comentario y agrega al arreglo temporal
+    const newComment = {
+      product: "Producto Actual", // Puedes ajustar esto según tu necesidad
+      description: commentText,
+      user: usernameLocal, // Puedes ajustar esto según tu necesidad
+      score: parseInt(score),
+      dateTime: new Date().toLocaleString(),
+    };
 
-        const score = document.getElementById("score").value;
-        const commentText = document.getElementById("comment").value;
+    commentsArray.push(newComment);
 
-        // Crea un nuevo comentario
-        const newComment = {
-            product: "Producto Actual", // Puedes ajustar esto según tu necesidad
-            description: commentText,
-            user: "Usuario Actual", // Puedes ajustar esto según tu necesidad
-            score: parseInt(score),
-            dateTime: new Date().toLocaleString(), // Fecha y hora actual
-        };
+// Vacía el campo de texto
+   document.getElementById("comment").value = ""; // Vaciar el campo de texto
 
-        // Envía el nuevo comentario a la API (puedes ajustar la URL según tu necesidad)
-        const apiUrlForNewComment = "URL_DE_TU_API_PARA_GUARDAR_COMENTARIOS";
-        fetch(apiUrlForNewComment, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newComment),
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log("Nuevo comentario agregado:", data);
-            // Recarga la lista de comentarios después de agregar uno nuevo
-            displayComments();
-        })
-        .catch((error) => {
-            console.error("Error al agregar comentario:", error);
-        });
+   
+    // Muestra los comentarios actualizados en la página
+    displayComment(newComment);
 
-        // Limpia el formulario después de enviar el comentario
-        commentForm.reset();
-    });
-
-    // ...
-});
-
-});
+  });
