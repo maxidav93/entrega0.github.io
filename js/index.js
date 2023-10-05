51cf3d492e13ccce06833f14a20629ef6db6e667
juanile <juanile97@gmail.com>
2023-10-05 15:24:48 -0300

    Actualizacion de código dark modo sin bugs

--- js/index.js
+++ js/index.js
@@ -53,7 +53,9 @@ function cambiarClase() {
          btn.classList.remove("btn-light");
          btn.classList.add("btn-dark");
     }
-    checkbox.checked = false;
+    setTimeout(function() {
+        checkbox.checked = false;
+    }, 100);
 }
 
 var checkbox = document.getElementById("toggle");
@@ -75,9 +77,64 @@ function cambiarClase2() {
         btn.classList.remove("btn-dark");
         btn.classList.add("btn-light");
     }
-    checkbox.checked = false;
+    setTimeout(function() {
+        checkbox.checked = false;
+    }, 100); // Después de 1000ms (1 segundo), desmarcar el checkbox
 }
 
 var checkbox2 = document.getElementById("toggle2");
 checkbox2.addEventListener("click", cambiarClase2);
+
+// Función para restaurar el estado del modo oscuro almacenado en localStorage
+function restaurarEstadoModoOscuro() {
+    var albumDiv = document.querySelector(".album");
+    var jumbotron = document.querySelector(".jumbotron");
+    var btn = document.querySelector(".btn.btn-light.btn-lg.btn-block");
+    var checkbox = document.getElementById("toggle");
+
+    // Verificar si el modo oscuro está habilitado en localStorage
+    var modoOscuroActivado = localStorage.getItem("background") === "bg-dark";
+
+    // Establecer el estado del checkbox según el modo oscuro
+    checkbox.checked = modoOscuroActivado;
+
+    if (modoOscuroActivado) {
+        // Si el modo oscuro está habilitado, aplicar los estilos oscuros
+        albumDiv.classList.remove("bg-light");
+        albumDiv.classList.add("bg-dark");
+        // Cambiar el color de fondo del .jumbotron
+        jumbotron.classList.remove("bg-light");
+        jumbotron.classList.add("bg-dark");
+        // Cambiar la clase del botón
+        btn.classList.remove("btn-light");
+        btn.classList.add("btn-dark");
+        // Llamar a la función para cambiar la imagen de fondo
+        cambiarImagenFondo('img/Frame 2.png', '#212529');
+    } else {
+        // Si el modo oscuro no está habilitado, aplicar los estilos de luz
+        albumDiv.classList.remove("bg-dark");
+        albumDiv.classList.add("bg-light");
+        // Cambiar el color de fondo del .jumbotron
+        jumbotron.classList.remove("bg-dark");
+        jumbotron.classList.add("bg-light");
+        // Cambiar la clase del botón
+        btn.classList.remove("btn-dark");
+        btn.classList.add("btn-light");
+        // Llamar a la función para cambiar la imagen de fondo
+        cambiarImagenFondo('img/cover_back.png');
+    }
+
+    // Desactivar el checkbox después de 1 segundo
+    setTimeout(function () {
+        checkbox.checked = false;
+    }, 100);
+}
+
+// Llama a la función para restaurar el estado del modo oscuro cuando la página se carga
+window.addEventListener("load", restaurarEstadoModoOscuro);
+
+
+
+
+
 
