ec3a27171ce922612c3f75428a295d4c48b59936
juanile <juanile97@gmail.com>
2023-10-21 15:44:24 -0300

    1

--- js/cart.js
+++ js/cart.js
@@ -2,22 +2,24 @@ document.addEventListener("DOMContentLoaded", () => {
   mostrarInformacionEnHTML();
 });
 
-  function mostrarInformacionEnHTML(data) {
-    const carritoContainer = document.getElementById('carritoContainer');
+function mostrarInformacionEnHTML(data) {
+  const carritoContainer = document.getElementById('carritoContainer');
+  const subtotalTotalValue = document.getElementById('subtotalTotalValue'); // Elemento donde se mostrará el subtotal total
 
-    const carritoActual = JSON.parse(localStorage.getItem("carrito"))
+  const carritoActual = JSON.parse(localStorage.getItem("carrito"))
 
-    if (!carritoActual || carritoActual.length === 0) {
-      carritoContainer.innerHTML = '<p class="alert alert-warning">El carrito está vacío</p>';
-      return;
-    }
+  if (!carritoActual || carritoActual.length === 0) {
+    carritoContainer.innerHTML = '<p class="alert alert-warning">El carrito está vacío</p>';
+    subtotalTotalValue.textContent = '$0.00'; // Establecer el subtotal total a $0.00 si el carrito está vacío
+    return;
+  }
 
-    const tableContainer = document.createElement("div");
-    tableContainer.classList.add("table-responsive");
+  const tableContainer = document.createElement("div");
+  tableContainer.classList.add("table-responsive");
 
-    const tabla = document.createElement('table');
-    tabla.classList.add('table', 'table-striped','table-bordered', 'w-100', 'table-responsive');
-    tabla.innerHTML = `
+  const tabla = document.createElement('table');
+  tabla.classList.add('table', 'table-striped', 'table-bordered', 'w-100', 'table-responsive');
+  tabla.innerHTML = `
       <thead class="thead-dark text-center">
         <tr>
           <th>Producto</th>
@@ -50,26 +52,26 @@ document.addEventListener("DOMContentLoaded", () => {
         <td>${producto.unitCost}</td>
         <td>${producto.currency}</td>
         `;
-  
+
         tbody.appendChild(fila);
         fila.appendChild(subtotalCell);
 
         const accionesCell = document.createElement('td');
         accionesCell.innerHTML = `
-          
+
            <svg onclick="borrarProducto(${producto.id})" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
          </svg>
-        
+
         `;
         fila.appendChild(accionesCell);
-  
+
         //TODO: modificar para que se actualice el localStorage
         const actualizarSubtotal = () => {
-  
+
           const cantidadInput = fila.querySelector(".cantidad");
           const cantidad = parseInt(cantidadInput.value);
-  
+
           const subtotal = cantidad * producto.unitCost;
           subtotalCell.textContent = subtotal;
         };
@@ -77,9 +79,8 @@ document.addEventListener("DOMContentLoaded", () => {
         fila.querySelector(".cantidad").addEventListener("change", actualizarSubtotal);
         actualizarSubtotal();
       });
-    
+
       tableContainer.appendChild(tabla);
       carritoContainer.innerHTML = "";
       carritoContainer.appendChild(tableContainer);
     }
-    
