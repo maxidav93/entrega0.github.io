9e7520c08c8efe3205e0bbc10284befcb918fdf6
juanile <juanile97@gmail.com>
2023-10-21 22:10:45 -0300

    1

--- js/cart3.js
+++ js/cart3.js
@@ -6,58 +6,71 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function mostrarInformacionEnHTML(data) {
-  if (!carritoActual || carritoActual.length === 0) {
-    carritoContainer.innerHTML = '<p class="alert alert-warning">El carrito está vacío</p>';
-    return;
-  }
+    if (!carritoActual || carritoActual.length === 0) {
+      carritoContainer.innerHTML = '<p class="alert alert-warning">El carrito está vacío</p>';
+      return;
+    }
 
-  const tableContainer = document.createElement("div");
-  tableContainer.classList.add("table-responsive");
+    const tableContainer = document.createElement("div");
+    tableContainer.classList.add("table-responsive");
 
-  const tabla = document.createElement('table');
-  tabla.classList.add('table', 'table-striped', 'table-bordered', 'w-100', 'table-responsive');
-  tabla.innerHTML = `
-    <thead class="thead-dark text-center">
-      <tr>
-        <th>Producto</th>
-        <th>Nombre</th>
-        <th class="col-1">Cantidad</th>
-        <th>Costo</th>
-        <th>Moneda</th>
-        <th>Subtotal</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody class="text-center">
-      <!-- Los datos del carrito se agregarán aquí dinámicamente -->
-    </tbody>
-  `;
-
-  // Obtener el cuerpo de la tabla para agregar filas
-  const tbody = tabla.querySelector('tbody');
-
-  carritoActual.forEach(producto => {
-    const fila = document.createElement("tr");
-    const subtotalCell = document.createElement("td");
-
-    fila.innerHTML = `
-      <td><img src="${producto.image}" alt="${producto.name}" style="width: 70px;"></td>
-      <td>${producto.name}</td>
-      <td class="col-1">
-        <input type="number" class="btn btn-sm cantidad" value="${producto.count}" min="1" data-producto-id="${producto.id}">
-      </td>
-      <td>${producto.unitCost}</td>
-      <td>${producto.currency}</td>
-      <td>
-      <button class="btn btn-danger btn-sm eliminar-producto" data-producto-id="${producto.id}">
-        Eliminar
-      </button>
-    </td>
+    const tabla = document.createElement('table');
+    tabla.classList.add('table', 'table-striped', 'table-bordered', 'w-100', 'table-responsive');
+    tabla.innerHTML = `
+      <thead class="thead-dark text-center">
+        <tr>
+          <th>Producto</th>
+          <th>Nombre</th>
+          <th class="col-1">Cantidad</th>
+          <th>Costo</th>
+          <th>Moneda</th>
+          <th>Subtotal</th>
+          <th>Acción</th> <!-- Agregamos la columna de acción -->
+        </tr>
+      </thead>
+      <tbody class="text-center">
+        <!-- Los datos del carrito se agregarán aquí dinámicamente -->
+      </tbody>
     `;
 
-    tbody.appendChild(fila);
-    fila.appendChild(subtotalCell);
+    // Obtener el cuerpo de la tabla para agregar filas
+    const tbody = tabla.querySelector('tbody');
 
+    carritoActual.forEach(producto => {
+      const fila = document.createElement("tr");
+      const subtotalCell = document.createElement("td");
+
+      fila.innerHTML = `
+        <td><img src="${producto.image}" alt="${producto.name}" style="width: 70px;"></td>
+        <td>${producto.name}</td>
+        <td class="col-1">
+          <input type="number" class="btn btn-sm cantidad" value="${producto.count}" min="1" data-producto-id="${producto.id}">
+        </td>
+        <td>${producto.unitCost}</td>
+        <td>${producto.currency}</td>
+      `;
+
+      // Agregamos un botón de eliminación a la fila
+      const eliminarButton = document.createElement("button");
+      eliminarButton.textContent = "Eliminar";
+      eliminarButton.classList.add("btn", "btn-danger", "btn-sm");
+      eliminarButton.dataset.productoId = producto.id;
+
+      // Función para eliminar un producto
+      eliminarButton.addEventListener("click", () => {
+        const productoId = producto.id;
+        carritoActual = carritoActual.filter(item => item.id !== productoId);
+        localStorage.setItem('carrito', JSON.stringify(carritoActual));
+        mostrarInformacionEnHTML();
+      });
+
+      // Agregamos el botón de eliminación a la fila
+      const accionCell = document.createElement("td");
+      accionCell.appendChild(eliminarButton);
+      fila.appendChild(accionCell);
+
+      tbody.appendChild(fila);
+      fila.appendChild(subtotalCell);
     // Función para actualizar el subtotal
     const actualizarSubtotal = () => {
       const cantidadInput = fila.querySelector(".cantidad");
@@ -101,7 +114,3 @@ function mostrarCosto() {
   });
   document.getElementById("subtotalCosto").textContent = `${parseFloat(subtotal).toFixed(2)}`;
 }
-
-
-
-
