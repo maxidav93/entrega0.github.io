faa3456a6e51e89c442d3835d902d3c7e8444adc
juanile <juanile97@gmail.com>
2023-10-21 21:26:33 -0300

    1

--- js/cart.js
+++ js/cart.js
@@ -1,126 +1,127 @@
+let carritoContainer = document.getElementById('carritoContainer');
+let carritoActual = JSON.parse(localStorage.getItem('carrito')) || [];
+
 document.addEventListener("DOMContentLoaded", () => {
-  mostrarInformacionEnHTML();
+    mostrarInformacionEnHTML();
 });
 
 function mostrarInformacionEnHTML(data) {
-  const carritoContainer = document.getElementById('carritoContainer');
+    if (!carritoActual || carritoActual.length === 0) {
+        carritoContainer.innerHTML = '<p class="alert alert-warning">El carrito está vacío</p>';
+        return;
+    }
 
-  const carritoActual = JSON.parse(localStorage.getItem("carrito"))
+    const tableContainer = document.createElement("div");
+    tableContainer.classList.add("table-responsive");
 
-  if (!carritoActual || carritoActual.length === 0) {
-    carritoContainer.innerHTML = '<p class="alert alert-warning">El carrito está vacío</p>';
-    return;
-  }
-
-  const tableContainer = document.createElement("div");
-  tableContainer.classList.add("table-responsive");
-
-  const tabla = document.createElement('table');
-  tabla.classList.add('table', 'table-striped', 'table-bordered', 'w-100', 'table-responsive');
-  tabla.innerHTML = `
-      <thead class="thead-dark text-center">
-        <tr>
-          <th>Producto</th>
-          <th>Nombre</th>
-          <th class="col-1">Cantidad</th>
-          <th>Costo</th>
-          <th>Moneda</th>
-          <th>Subtotal</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody class="text-center">
-        <!-- Los datos del carrito se agregarán aquí dinámicamente -->
-      </tbody>
+    const tabla = document.createElement('table');
+    tabla.classList.add('table', 'table-striped', 'table-bordered', 'w-100', 'table-responsive');
+    tabla.innerHTML = `
+        <thead class="thead-dark text-center">
+            <tr>
+                <th>Producto</th>
+                <th>Nombre</th>
+                <th class="col-1">Cantidad</th>
+                <th>Costo</th>
+                <th>Moneda</th>
+                <th>Subtotal</th>
+                <th>Eliminar</th>
+            </tr>
+        </thead>
+        <tbody class="text-center">
+            <!-- Los datos del carrito se agregarán aquí dinámicamente -->
+        </tbody>
     `;
 
-  // Obtener el cuerpo de la tabla para agregar filas
-  const tbody = tabla.querySelector('tbody');
+    const tbody = tabla.querySelector('tbody');
 
+    carritoActual.forEach(producto => {
+        const fila = crearFilaProducto(producto);
+        tbody.appendChild(fila);
+    });
 
-  carritoActual.forEach(producto => {
-    const fila = document.createElement("tr");
-    const subtotalCell = document.createElement("td");
-    fila.innerHTML = `
-        <td><img src="${producto.image}" alt="${producto.name}" style="width: 70px;"></td>
-        <td>${producto.name}</td>
-        <td class="col-1">
-        <input type="number" class="btn btn-sm cantidad" value="${producto.count}" min="1" data-producto-id="${producto.id}">
-        </td>
-        <td>${producto.unitCost}</td>
-        <td>${producto.currency}</td>
-        `;
-
-    tbody.appendChild(fila);
-    fila.appendChild(subtotalCell);
-
-    //Boton de borrar
-
-    const accionesCell = document.createElement('td');
-    accionesCell.innerHTML = `
-
-           <i  onclick="borrarProducto(${producto.id})"   class='btn custom-delete-btn fas fa-trash-alt'
-         id="botonBorrar"
-           </i>
-
-        `;
-    fila.appendChild(accionesCell);
-    accionesCell.setAttribute("id" , "tdboton")
-
-
-    //TODO: modificar para que se actualice el localStorage
-    const actualizarSubtotal = () => {
-
-      const cantidadInput = fila.querySelector(".cantidad");
-      const cantidad = parseInt(cantidadInput.value);
-
-      const subtotal = cantidad * producto.unitCost;
-      subtotalCell.textContent = subtotal;
-    };
-
-    fila.querySelector(".cantidad").addEventListener("change", actualizarSubtotal);
-    actualizarSubtotal();
-  });
-
-  tableContainer.appendChild(tabla);
-  carritoContainer.innerHTML = "";
-  carritoContainer.appendChild(tableContainer);
+    tableContainer.appendChild(tabla);
+    carritoContainer.innerHTML = "";
+    carritoContainer.appendChild(tableContainer);
 }
-function eliminarProductoDelLocalStorage(id) {
-  // Obten el contenido actual del Local Storage
-  var productosEnLocalStorage = JSON.parse(localStorage.getItem('carrito')) || [];
 
-  // Filtra los productos para eliminar el que coincide con el ID
-  productosEnLocalStorage = productosEnLocalStorage.filter(function(producto) {
-      return producto.id !== id;
-  });
+function crearFilaProducto(producto) {
+    const fila = document.createElement("tr");
 
-  // Actualiza el Local Storage con la nueva lista de productos
-  localStorage.setItem('carrito', JSON.stringify(productosEnLocalStorage));
-};
+    const imagenCell = document.createElement("td");
+    imagenCell.innerHTML = `<img src="${producto.image}" alt="${producto.name}" style="width: 70px;">`;
 
+    const nombreCell = document.createElement("td");
+    nombreCell.textContent = producto.name;
 
-  function borrarProducto(id) {
-        // Muestra una alerta de confirmación
-        var confirmacion = confirm('¿Estás seguro de que deseas eliminar este producto?');
+    const cantidadCell = document.createElement("td");
+    cantidadCell.innerHTML = `
+        <input type="number" class="btn btn-sm cantidad" value="${producto.count}" min="1" data-producto-id="${producto.id}">
+    `;
 
-        // Si el usuario hace clic en "Aceptar" en la alerta de confirmación
-        if (confirmacion) {
+    const costoCell = document.createElement("td");
+    costoCell.textContent = producto.unitCost;
 
-    // Encuentra el elemento SVG haciendo referencia al ícono del bote de basura
-    var iElement = document.querySelector('i[onclick="borrarProducto(' + id + ')"]');
+    const monedaCell = document.createElement("td");
+    monedaCell.textContent = producto.currency;
 
-    // Encuentra el elemento padre del SVG
-    var parentElement = iElement.parentElement;
+    const subtotalCell = document.createElement("td");
 
-    // Encuentra el elemento padre de la fila que contiene el ícono del bote de basura
-    var rowElement = parentElement.parentElement;
+    const eliminarCell = document.createElement("td");
+    eliminarCell.innerHTML = `
+        <button class="btn btn-danger" data-producto-id="${producto.id}">Eliminar</button>
+    `;
 
-    // Elimina la fila completa
-    rowElement.remove();
-    // Elimina el producto del Local Storage
-    eliminarProductoDelLocalStorage(id);
-}};
+    fila.appendChild(imagenCell);
+    fila.appendChild(nombreCell);
+    fila.appendChild(cantidadCell);
+    fila.appendChild(costoCell);
+    fila.appendChild(monedaCell);
+    fila.appendChild(subtotalCell);
+    fila.appendChild(eliminarCell);
 
+    cantidadCell.querySelector(".cantidad").addEventListener("change", () => {
+        actualizarSubtotal(fila, producto);
+    });
 
+    eliminarCell.querySelector("button").addEventListener("click", () => {
+        eliminarProducto(producto.id);
+    });
 
+    actualizarSubtotal(fila, producto);
+
+    return fila;
+}
+
+function actualizarSubtotal(fila, producto) {
+    const cantidadInput = fila.querySelector(".cantidad");
+    const cantidad = parseInt(cantidadInput.value);
+    const subtotal = fila.querySelector("td:last-child");
+    if (producto.currency === 'UYU') {
+        const subtotalValue = cantidad * producto.unitCost * 40;
+        subtotal.textContent = `UYU ${subtotalValue}`;
+    } else {
+        const subtotalValue = cantidad * producto.unitCost;
+        subtotal.textContent = `${producto.currency} ${(subtotalValue).toFixed(2)}`;
+    }
+    displayCosts();
+}
+
+function eliminarProducto(id) {
+    carritoActual = carritoActual.filter(producto => producto.id !== id);
+    localStorage.setItem("carrito", JSON.stringify(carritoActual));
+    mostrarInformacionEnHTML();
+}
+
+function displayCosts() {
+    let subtotal = 0;
+    carritoActual.forEach(producto => {
+        if (producto.currency === 'UYU') {
+            subtotal += producto.count * producto.unitCost * 40;
+        } else {
+            subtotal += producto.count * producto.unitCost;
+        }
+    }
+    );
+    document.getElementById("subtotalCosto").textContent = `${parseFloat(subtotal).toFixed(2)}`;
+}
